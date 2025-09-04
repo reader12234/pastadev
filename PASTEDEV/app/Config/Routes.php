18ff98845c3a2290b964de4ec4363fe8d220c9bc
James Dominic A. Segubiense <jasegubiense@my.cspc.edu.ph>
2025-09-04 08:17:28 +0800

    test

---
+++ PASTEDEV/app/Config/Routes.php
@@ -0,0 +1,10 @@
+<?php
+
+use CodeIgniter\Router\RouteCollection;
+
+/**
+ * @var RouteCollection $routes
+ */
+$routes->get('/', 'Home::index');
+$routes->get('Register', 'Register::index');
+$routes->get('Login', 'Login::index');
